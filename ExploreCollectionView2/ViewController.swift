4021aed6edd4cacd114fe78f6829f1314a5b36a2
Sergio <afirthes@gmail.com>
2022-10-29 18:30:49 +0300

    Initial Commit

---
+++ ExploreCollectionView2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExploreCollectionView2
+//
+//  Created by Afir Thes on 29.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
